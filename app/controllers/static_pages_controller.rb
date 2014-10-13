639db953e924be72ec3e4ec68fc8266fa6ccaf57
stevegocoding <stevegocoding@gmail.com>
2014-10-12 22:27:41 -0400

    add user controller and sign-up route

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    puts "StaticPagesController#home"
   end
 
   def help
+    puts "StaticPagesController#help"
   end
 
   def about
+    puts "StaticPagesController#about"
+  end
+  
+  def contact
+    puts "StaticPagesController#contact"
   end
 end
