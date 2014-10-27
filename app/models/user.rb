fa98d5aca859cd7bb08bdb1871f4b7a0d68b6437
stevegocoding <stevegocoding@gmail.com>
2014-10-27 09:56:45 -0400

    add microposts model the sample data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   # Validation
   validates :name, presence: true, length: { maximum: 50 } 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
